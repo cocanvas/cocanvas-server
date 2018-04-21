0320cd3940c12f586190893396ef2381c938a609
Mandy Tang <mandytang@mandys-mbp.home>
2018-04-21 13:33:59 +1000

    pre-pull commit

--- app/models/coordinate.rb
+++ app/models/coordinate.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: coordinates
+#
+#  id         :integer          not null, primary key
+#  colour     :string
+#  x          :integer
+#  y          :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Coordinate < ApplicationRecord
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
+# == Route Map
+#
+#          Prefix Verb   URI Pattern                     Controller#Action
+#     coordinates GET    /coordinates(.:format)          coordinates#index
+#                 POST   /coordinates(.:format)          coordinates#create
+#  new_coordinate GET    /coordinates/new(.:format)      coordinates#new
+# edit_coordinate GET    /coordinates/:id/edit(.:format) coordinates#edit
+#      coordinate GET    /coordinates/:id(.:format)      coordinates#show
+#                 PATCH  /coordinates/:id(.:format)      coordinates#update
+#                 PUT    /coordinates/:id(.:format)      coordinates#update
+#                 DELETE /coordinates/:id(.:format)      coordinates#destroy
+#           users GET    /users(.:format)                users#index
+#                 POST   /users(.:format)                users#create
+#        new_user GET    /users/new(.:format)            users#new
+#       edit_user GET    /users/:id/edit(.:format)       users#edit
+#            user GET    /users/:id(.:format)            users#show
+#                 PATCH  /users/:id(.:format)            users#update
+#                 PUT    /users/:id(.:format)            users#update
+#                 DELETE /users/:id(.:format)            users#destroy
+#
+
 Rails.application.routes.draw do
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#Destroy'
+
   resources :coordinates
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
