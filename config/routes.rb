82427e6af96ad3599424403245c6986466061832
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-21 12:06:03 +1000

    trialling sending coords data via ajax request from within backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  root to: 'pages#index'
   resources :coordinates
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
