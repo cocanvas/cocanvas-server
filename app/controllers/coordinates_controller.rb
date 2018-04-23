88bf336088220bd6302036da4b1dd2f54d07055b
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-23 15:11:12 +1000

    testing if statement logic for grabbing only 4800 coords

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -5,8 +5,8 @@ class CoordinatesController < ApplicationController
   def index
     # @coordinates = Coordinate.all
     @coords_num = Coordinate.count
-    if @coords_num >= 4801
-      @latest_coords = Coordinate.order('created_at DESC').limit(4800).reverse
+    if @coords_num >= 20
+      @latest_coords = Coordinate.order('created_at DESC').limit(20).reverse
     else
       @latest_coords = Coordinate.all
     end
