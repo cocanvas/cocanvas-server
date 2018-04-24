7d0cefadff8e55b79ec9534bb6474bb5150d1682
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-24 22:52:11 +1000

    trying to fix issue created after changing ports

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,8 +61,7 @@ Rails.application.configure do
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://127.0.0.1:5000',
-    'http://localhost:5000',
-    'http://127.0.0.1:49272'
+    'http://localhost:5000'
   ]
 
 end
