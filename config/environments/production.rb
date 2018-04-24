535f9484504832be519fbd00f7a70a6d5339cd6d
robo2323 <rob.adam.h@gmail.com>
2018-04-24 11:38:41 +1000

    redis config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
 
   config.action_cable.url = 'https://cocanvas-server.herokuapp.com/cable'
 
-  config.action_cable.allowed_request_origins = ['https://cocanvas-server.herokuapp.com, https://amandytang.github.io/cocanvas-js', 'http://amandytang.github.io/cocanvas-js']
+  config.action_cable.allowed_request_origins = ['https://cocanvas-server.herokuapp.com, https://amandytang.github.io/cocanvas-js', 'http://amandytang.github.io/cocanvas-js','http://localhost:5500']
 end
