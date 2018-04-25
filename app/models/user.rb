0982a73425a646c9de7a2019c0913de4fa00f787
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-26 09:45:26 +1000

    updating messgaes to chatmessages everywhere to fix things

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ApplicationRecord
   has_many :coordinates
-  has_many :messages
+  has_many :chat_messages
   has_secure_password
   validates :username, :presence => true, :uniqueness => true
   validates :password_digest, presence: true, confirmation: true
