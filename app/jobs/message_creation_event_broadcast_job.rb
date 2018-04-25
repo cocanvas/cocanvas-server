0565c230407fa3972e3dbf3d7e81550461aeda71
Katrina Hayes <katrinamargaret.hayes@gmail.com>
2018-04-25 17:59:29 +1000

    change to strftime format

--- app/jobs/message_creation_event_broadcast_job.rb
+++ app/jobs/message_creation_event_broadcast_job.rb
@@ -9,7 +9,7 @@ class MessageCreationEventBroadcastJob < ApplicationJob
         # user_id: msg.user_id,
         username: User.find_by_id(msg.user_id).username,
         content: msg.content,
-        created_at: msg.created_at.strftime('%H:%M:%S')
+        created_at: msg.created_at.strftime('%H:%M')
       )
   end
 end
